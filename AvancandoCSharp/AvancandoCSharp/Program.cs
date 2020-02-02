245193fbf1ab56cc1631b446eaa6a0b7e5a30175
Cleber Santos <oclebersantos@gmail.com>
2020-02-02 12:57:14 -0300

    first commit

---
+++ AvancandoCSharp/AvancandoCSharp/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using static System.Console;
+namespace AvancandoCSharp
+{
+   
+    public enum TipoCliente
+    {
+        PessoaFisica, PessoaJuridica, OrgaoPublico, ONG
+    }
+
+    public enum StatusPedido
+    {
+        AguardandoPagamento = 1,
+        Aprovado = 2,
+        Enviado = 3
+    }
+
+    // nao faz heranca, pode fazer implementacao de interface, ocupa menos espaco
+    public struct Cliente
+    {
+        public int matricula;
+        public string nome;
+        public string email;
+
+        public void digaOi()
+        {
+            WriteLine($" oi {this.nome}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Carro carro = new Carro();
+            //carro.Marca = "Fusca";
+            //carro.AnoFabricacao = 1998;
+
+            //WriteLine($"Eu tenho um {carro.Marca} fabricado em {carro.AnoFabricacao}");
+            //carro.buzinar();
+            //carro.virar("D");
+            //StatusPedido.AguardandoPagamento;
+
+            //string frase = "Hoje à noite, sem luz, decidi xeretar a quinta gaveta de vovô: achei lingüiça, pão e fubá";
+            //string[] partes = frase.Split(",");
+
+            //for(var i = 0; i < partes.Length; i++)
+            //{
+            //    string parte = partes[i].Trim();
+            //bool possuiX = parte.Contains("x");
+            //WriteLine($"Possui X {possuiX}");
+            //int posicaoInicial = parte.IndexOf("x");
+            //int posicaoFinal = 10;
+
+            //string sub = frase.Substring(posicaoInicial, posicaoFinal);
+            //WriteLine(parte.ToLower());
+            //WriteLine(parte.ToUpper());
+            //WriteLine(parte.Replace("Xeretar", "investigar"));
+            //WriteLine(parte.Replace("Xeretar", "investigar", true , CultureInfo.CurrentCulture));
+
+            //};
+            //string nome = "Cleber Santos";
+            //int idade = 31;
+            //double rendimento = 80000.00;
+            //DateTime dataCadastro = new DateTime(2020, 02, 01);
+
+
+            //string str = "O Cliente {0} tem {1} anos de idade e tem rendimento de {2:c}";
+            //str += " e é cliente desde {3:dd/MM/yyyy}";
+            //string frase = String.Format(str, nome, idade, rendimento, dataCadastro);
+            //StringBuilder builder = new StringBuilder("Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
+            //builder.Append("Nunc tempus leo sit amet placerat tempus.");
+            //builder.AppendLine("Morbi accumsan mauris diam, nec porta lorem interdum sed.");
+            //builder.Append("Pellentesque urna lacus, porta in lobortis volutpat, ultrices non turpis.");
+            //builder.AppendFormat(nome, idade, rendimento, dataCadastro);
+
+            //WriteLine("{0}", DateTime.Now.AddDays(2));
+            //WriteLine("{0}", DateTime.Now.AddDays(-2));
+            //DateTime data = new DateTime(2018, 04, 02, 08, 30, 00);
+            //WriteLine(data);
+
+            //string dataDoBanco = "2018-04-02 08:30:00";
+
+            //DateTime data = DateTime.Parse(dataDoBanco);
+            //string horaFormatada = String.Format("{0:HH}h{0:mm}", data);
+            //string dataFormatada = String.Format("{0:dd/MM/yyyy}", data);
+            //WriteLine(horaFormatada);
+
+
+            ReadLine();
+
+
+            //WriteLine(builder);
+        }
+    }
+
+    class Carro
+    {
+        private string _marca;
+        public string Marca
+        {
+            get => _marca;
+            set => _marca = value.Equals("Fusca") ? "OUTRO CARRO" : value;
+
+            //get { return _marca;  }
+            //set { _marca = value; }
+        }
+        //public string Marca { get; set;  }
+
+        private int _anoFabricacao;
+        public int AnoFabricacao
+        {
+            get => _anoFabricacao;
+            set => _anoFabricacao = value;
+        }
+
+        public void buzinar()
+        {
+            WriteLine("Bi bi");
+        }
+
+        public void virar(string direcao)
+        {
+            if (direcao.Equals("D"))
+            {
+                WriteLine("Virando a direita ...");
+            }
+            else
+            {
+                WriteLine("Virando a equerda ...");
+            }
+        }
+
+    }
+}
---
+++ CSharpAdoNet/CSharpAdoNet/Program.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Data.SqlClient;
+using static System.Console;
+
+namespace CSharpAdoNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("=============== CONTROLE DE CLIENTES =================");
+            WriteLine("Selecione uma opção");
+            WriteLine("1 - Listar");
+            WriteLine("2 - Novo");
+            WriteLine("3 - Editar");
+            WriteLine("4 - Excluir");
+            WriteLine("5 - Visualizar");
+            Write("Opção: ");
+            int opc = Convert.ToInt32(ReadLine());
+
+            string nome = "";
+            string email = "";
+
+            switch (opc)
+            {
+                case 1:
+                    Title = "Listagem de Clientes";
+                    WriteLine("=======================LISTAGEM DE CLIENTES=================");
+                    ListarClientes();
+                    break;
+                case 2:
+                    Title = "Novos Clientes";
+                    WriteLine("=======================CADASTRO NOVO CLIENTE=================");
+                    Write("Informe um nome: ");
+                    nome = ReadLine();
+
+                    Write("Informe um e-mail: ");
+                    email = ReadLine();
+                    SalvarCliente(nome, email);
+                    break;
+                case 3:
+                    Title = "Alteração de Cliente";
+                    WriteLine("=======================ALTERACAO CLIENTE=================");
+                    ListarClientes();
+                    Write("Selecione um cliente pela ID: ");
+                    int idOp = Convert.ToInt32(ReadLine());
+
+                    (int _id, string _nome, string _email) = SelecionarCliente(idOp);
+
+                    Clear();
+                    Title = "Alteração de Cliente - " + _nome;
+                    WriteLine($"=======================ALTERACAO CLIENTE - {_nome}=================");
+
+                    Write("Informe o novo nome: ");
+                    nome = ReadLine();
+
+                    Write("Informe o novo e-mail: ");
+                    email = ReadLine();
+
+                    nome = nome.Equals("") ? _nome : nome;
+                    email = email.Equals("") ? _email : email;
+
+                    SalvarCliente(nome, email, idOp);
+
+                    ListarClientes();
+                    break;
+                case 4:
+                    Title = "Exclusão de Cliente";
+                    WriteLine("=======================ALTERACAO CLIENTE=================");
+
+                    ListarClientes();
+                    Write("Selecione um cliente pela ID: ");
+                    idOp = Convert.ToInt32(ReadLine());
+
+                    (_id, _nome, _email) = SelecionarCliente(idOp);
+
+                    Clear();
+
+                    Title = "Exclusão de Cliente - " + _nome;
+                    WriteLine($"=======================EXCLUSAO DE CLIENTE - {_nome}=================");
+
+                    WriteLine("\n*************** ATENÇÃO ************\n");
+                    Write("Deseja continuar? (S para SIM, N para NÃO) ");
+                    string confirmar = ReadLine().ToUpper();
+
+                    if (confirmar.Equals("S"))
+                    {
+                        DeletarCliente(idOp);
+                        ListarClientes();
+                    }
+                    break;
+                case 5:
+                    Title = "Visualização de Cliente";
+                    WriteLine("=======================VER DETALHES DE CLIENTE=================");
+
+                    ListarClientes();
+                    Write("Selecione um cliente pela ID: ");
+                    idOp = Convert.ToInt32(ReadLine());
+
+                    (_id, _nome, _email) = SelecionarCliente(idOp);
+
+                    Clear();
+
+                    Title = "Visualização de Cliente - " + _nome;
+                    WriteLine($"=======================DETALHES DO CLIENTE - {_nome}=================");
+
+                    WriteLine("ID: {0}", _id);
+                    WriteLine("Nome: {0}", _nome);
+                    WriteLine("Email: {0}", _email);
+                    break;
+                default:
+                    Title = "Opção inválida";
+                    WriteLine("=======================SELECIONE UM OPÇÃO VÁLIDA=================");
+                    break;
+
+            }
+        }
+
+        // tupla c# 7
+        static (int, string, string) SelecionarCliente(int id)
+        {
+            string connString = getStringConn();
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "select * from clientes where id = @id";
+                cmd.Parameters.AddWithValue("@id", id);
+
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    dr.Read();
+                    return (
+                        Convert.ToInt32(dr["id"].ToString()),
+                        dr["nome"].ToString(),
+                        dr["email"].ToString()
+                    );
+                }
+            }
+
+        }
+
+
+        static void ListarClientes()
+        {
+            string connString = getStringConn();
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "select * from clientes order by id";
+
+                using(SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        WriteLine("ID: {0}", dr["id"]);
+                        WriteLine("Nome: {0}", dr["nome"]);
+                        WriteLine("----------------------");
+
+                    }
+                }
+            }
+        }
+
+        static void SalvarCliente(string nome, string email)
+        {
+            string connString = getStringConn();
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "insert into clientes(nome, email) values (@nome, @email)";
+                cmd.Parameters.AddWithValue("@nome", nome);
+                cmd.Parameters.AddWithValue("@email", email);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        static void SalvarCliente(string nome, string email, int id)
+        {
+            string connString = getStringConn();
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "update clientes set nome = @nome, email = @email where id = @id";
+                cmd.Parameters.AddWithValue("@nome", nome);
+                cmd.Parameters.AddWithValue("@email", email);
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        static void DeletarCliente(int id)
+        {
+            string connString = getStringConn();
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "delete from clientes where id = @id";
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+
+        static string getStringConn()
+        {
+            return @"
+               Server=127.0.0.1,1433;
+               Database=CSharpDotNet;
+               User Id=SA;
+               Password=estudo@123456
+            "; // conexao com container docker
+        }
+    }
+
+  
+}
